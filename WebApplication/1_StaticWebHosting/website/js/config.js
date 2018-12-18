f353d21532c14e06d529be6e1b760a8d326d232b
ABBZarKad <43981650+ABBZarKad@users.noreply.github.com>
2018-12-18 13:25:04 +0100

    updated code

--- WebApplication/1_StaticWebHosting/website/js/config.js
+++ WebApplication/1_StaticWebHosting/website/js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
         userPoolId: 'eu-west-1_lhu6EUzky', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '15s5mt2vmdiifebjilba8f5dhq' // e.g. us-east-2
+        userPoolClientId: '15s5mt2vmdiifebjilba8f5dhq', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'eu-west-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
