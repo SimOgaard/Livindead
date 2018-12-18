b1142a74d16c448610b3bdd4bcf56cc52a23397e
ABBZarKad <43981650+ABBZarKad@users.noreply.github.com>
2018-12-18 13:15:54 +0100

    unicorn1

--- WebApplication/1_StaticWebHosting/website/js/config.js
+++ WebApplication/1_StaticWebHosting/website/js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
+        userPoolId: 'eu-west-1_lhu6EUzky', // e.g. us-east-2_uXboG5pAb
         userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        region: '15s5mt2vmdiifebjilba8f5dhq' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
