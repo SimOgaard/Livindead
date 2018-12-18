7d3291c3af828aa08386f2bdf286b25b140a1e93
ABBZarKad <43981650+ABBZarKad@users.noreply.github.com>
2018-12-18 17:35:01 +0100

    g

--- WebApplication/1_StaticWebHosting/website/js/ride.js
+++ WebApplication/1_StaticWebHosting/website/js/ride.js
@@ -18,7 +18,7 @@ WildRydes.map = WildRydes.map || {};
     function requestUnicorn(pickupLocation) {
         $.ajax({
             method: 'POST',
-            url: _config.api.invokeUrl + '/ride/ride',
+            url: _config.api.invokeUrl + '/ride/Ride',
             headers: {
                 Authorization: authToken
             },
